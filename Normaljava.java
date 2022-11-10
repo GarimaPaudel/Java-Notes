899f3c5959796ce172d231f6c1ad815469b2cd2a
Garima <paudelgarima229@gmail.com>
2022-11-10 08:53:03 +0545

    first commit

---
+++ Normaljava.java
@@ -0,0 +1,31 @@
+
+public class Normaljava {
+
+ 
+    public static void main(String[] args) {
+        //Print
+ System.out.println("Hello world"); 
+ 
+ //Integer
+ int x = 5;
+ int y = 6;
+ System.out.println(x+y);
+ 
+ //Strings
+ String firstName = "Garima";
+ String lastName = "Paudel";
+ System.out.println(firstName +" " +lastName);
+ 
+ //float
+ float f1 = 2.05f;
+ float f2 = 5.06f;
+ System.out.println(f1+f2);
+ 
+ //char and boolean
+ char myLetter = 'D';
+boolean myBool = true;
+
+
+    }
+    
+}
